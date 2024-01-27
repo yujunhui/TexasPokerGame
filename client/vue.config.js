fce14fb48926fe9d26e9bdd651937448b32d3821
yujunhui <lyujunhui@qq.com>
2024-01-27 21:26:50 +0800

    feat: remove console.log in prod

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,4 +2,17 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
+  chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer('terser').tap((args) => {
+        Object.assign(args[0].terserOptions.compress, {
+          warnings: false,
+          drop_console: true,
+          drop_debugger: true,
+          pure_funcs: ['console.log'],
+        });
+        return args;
+      });
+    }
+  },
 };
