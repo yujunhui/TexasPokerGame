0ecb960f9e5f18c80802e85eb75e512fc73a8a94
yujunhui <lyujunhui@qq.com>
2024-01-27 23:39:38 +0800

    feat: optimize

--- scripts/docker_monitor_cpu.sh
+++ scripts/docker_monitor_cpu.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 CONTAINER_NAME="dzp-api"
-RESTART_THRESHOLD=3
 CPU_THRESHOLD=95
+RESTART_THRESHOLD=3
+INTERVAL=5
 
 count=0
+echo "---Start monitor ${CONTAINER_NAME} CPU usage(interval: ${INTERVAL}s, cpu threshold: ${CPU_THRESHOLD}%, restart count threshold: ${RESTART_THRESHOLD})"
 while true
 do
   cpu_usage=$(docker stats --no-stream --format "{{.CPUPerc}}" $CONTAINER_NAME | cut -d'%' -f1)
@@ -12,10 +14,10 @@ do
 
   if [ $cpu_usage -gt $CPU_THRESHOLD ]; then
     ((count++))
-    echo "CPU usage for $CONTAINER_NAME is above the threshold: $cpu_usage%"
+    echo "---CPU usage for $CONTAINER_NAME is above the threshold: $cpu_usage%, hits: ${count}/${RESTART_THRESHOLD}"
 
     if [ $count -eq $RESTART_THRESHOLD ]; then
-      echo "Restarting $CONTAINER_NAME..."
+      echo "!!!Restarting $CONTAINER_NAME..."
       docker restart $CONTAINER_NAME
       count=0  # 重置计数器
     fi
@@ -23,5 +25,5 @@ do
     count=0  # CPU 使用率低于阈值时，重置计数器
   fi
 
-  sleep 5
+  sleep ${INTERVAL}
 done
